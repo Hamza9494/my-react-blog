f565f91bb0bf1c74d93bde19807de229d53de56c
hamza <baitichhamza@gmail.com>
2023-09-23 17:53:59 +0100

    test git

--- src/Nvabr.js
+++ src/Nvabr.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <div className="navbar nav">
       <Link to={'/'}>
         <h2>my react blog</h2>
       </Link>
